6878ebe261b2aea34b251eb39b3c3165820b65ab
hp <hp@192.168.100.10>
2018-04-18 23:16:23 +0530

    authorities, customer&cart completd

---
+++ fossilbackend/src/main/java/com/niit/model/Cart.java
@@ -0,0 +1,38 @@
+package com.niit.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+@Entity
+public class Cart {
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+private int id;
+private double grandTotal;
+@OneToOne
+private Customer customer;//FK customer_id
+public Cart(){
+	System.out.println("Cart object is created");
+}
+public int getId() {
+	return id;
+}
+public void setId(int id) {
+	this.id = id;
+}
+public double getGrandTotal() {
+	return grandTotal;
+}
+public void setGrandTotal(double grandTotal) {
+	this.grandTotal = grandTotal;
+}
+public Customer getCustomer() {
+	return customer;
+}
+public void setCustomer(Customer customer) {
+	this.customer = customer;
+}
+
+}
